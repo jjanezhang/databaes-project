11414648f94fbadd4c8a685ecdd4ac64a21052ae
Roy Xiong <roy.xiong.1@gmail.com>
2022-02-28 00:46:33 -0500

    Allow adding products to inventory

--- app/index.py
+++ app/index.py
@@ -34,18 +34,32 @@ class QuantityForm(FlaskForm):
     quantity = IntegerField('Quantity', validators=[DataRequired(), NumberRange(min=0)])
     submit = SubmitField('Update Quantity')
 
+class AddProductForm(FlaskForm):
+    pid = SelectField('Product Name', validators=[DataRequired()])
+    quantity = IntegerField('Quantity', validators=[DataRequired(), NumberRange(min=0)])
+    submit = SubmitField('Add Product')
+
 @bp.route('/inventory', methods=['GET', 'POST'])
 def inventory():
     quantity_form = QuantityForm()
+    add_product_form = AddProductForm()
     if current_user.is_authenticated:
+        all_products = Product.get_all_regardless_of_availability()
         inventory = Inventory.get_all(current_user.id)
         quantity_form.pid.choices = [(product.pid, product.name) for product in inventory]
         quantity_form.pid.choices.sort()
+        all_products = filter(lambda product: product.id not in [x.pid for x in inventory], all_products)
+        add_product_form.pid.choices = [(product.id, product.name) for product in all_products]
+        add_product_form.pid.choices.sort()
         if quantity_form.validate_on_submit():
             if Inventory.update_item_quantity(current_user.id, quantity_form.pid.data, quantity_form.quantity.data):
+                return redirect(url_for('index.inventory'))
+        if add_product_form.validate_on_submit():
+            if Inventory.add_item(current_user.id, add_product_form.pid.data, add_product_form.quantity.data):
                 return redirect(url_for('index.inventory'))
     else:
         inventory = None
     return render_template('inventory.html', 
                             inventory=inventory,
+                            add_product_form=add_product_form,
                             quantity_form=quantity_form)

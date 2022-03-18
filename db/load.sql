79c1828bcc5fa23a343ebaae800ec6cd43662f5a
Roy Xiong <roy.xiong.1@gmail.com>
2022-03-18 17:31:42 -0400

    Update tables and initial load for Purchases and Orders

--- db/load.sql
+++ db/load.sql
@@ -10,7 +10,9 @@ SELECT pg_catalog.setval('public.products_id_seq',
                          (SELECT MAX(id)+1 FROM Products),
                          false);
 
-\COPY Purchases FROM 'Purchases.csv' WITH DELIMITER ',' NULL '' CSV
-SELECT pg_catalog.setval('public.purchases_id_seq',
-                         (SELECT MAX(id)+1 FROM Purchases),
+\COPY Orders FROM 'Orders.csv' WITH DELIMITER ',' NULL '' CSV
+SELECT pg_catalog.setval('public.orders_id_seq',
+                         (SELECT MAX(id)+1 FROM Products),
                          false);
+
+\COPY Purchases FROM 'Purchases.csv' WITH DELIMITER ',' NULL '' CSV

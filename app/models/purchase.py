4208f39e8ab0b3994d1d105c264a7389a4a442c1
Roy Xiong <roy.xiong.1@gmail.com>
2022-03-19 18:56:52 -0400

    Update purchase model

--- app/models/purchase.py
+++ app/models/purchase.py
@@ -2,31 +2,23 @@ from flask import current_app as app
 
 
 class Purchase:
-    def __init__(self, id, uid, pid, time_purchased):
-        self.id = id
-        self.uid = uid
+    def __init__(self, oid, pid, sid, fulfilled, time_fulfilled, price):
+        self.oid = oid
         self.pid = pid
-        self.time_purchased = time_purchased
-
-    @staticmethod
-    def get(id):
-        rows = app.db.execute('''
-SELECT id, uid, pid, time_purchased
-FROM Purchases
-WHERE id = :id
-''',
-                              id=id)
-        return Purchase(*(rows[0])) if rows else None
+        self.sid = sid
+        self.fulfilled = fulfilled
+        self.time_fulfilled = time_fulfilled
+        self.price = price
 
     @staticmethod
     def get_all_by_uid_since(uid, since):
         rows = app.db.execute('''
-SELECT id, uid, pid, time_purchased
-FROM Purchases
-WHERE uid = :uid
-AND time_purchased >= :since
-ORDER BY time_purchased DESC
-''',
-                              uid=uid,
-                              since=since)
+            SELECT P.oid AS oid, P.pid AS pid, P.sid AS sid, P.fulfilled AS fulfilled,
+            P.time_fulfilled AS time_fulfilled, P.price AS price
+            FROM Purchases P, Orders O
+            WHERE O.id = P.oid AND O.uid = :uid
+            AND O.time_placed >= :since
+            ORDER BY O.time_placed DESC
+            '''
+        , uid=uid, since=since)
         return [Purchase(*row) for row in rows]

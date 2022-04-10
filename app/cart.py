b720cda1d5568844073014d34c578eaff3da0d9c
Roy Xiong <roy.xiong.1@gmail.com>
2022-04-10 11:17:28 -0400

    Allow updating cart

--- app/cart.py
+++ app/cart.py
@@ -1,13 +1,43 @@
 from flask import Blueprint, render_template, redirect, url_for, request, flash
 from flask_login import current_user
 
+from flask_wtf import FlaskForm
+from wtforms import IntegerField, SelectField, SubmitField
+from wtforms.validators import DataRequired, NumberRange, InputRequired
+
+
 from .models.cart import Cart
 
 bp = Blueprint('cart', __name__, url_prefix='/cart')
 
+class QuantityForm(FlaskForm):
+    pid = SelectField('Product Name', validators=[DataRequired()])
+    sid = SelectField('Seller Name', validators=[DataRequired()])
+    new_quantity = IntegerField('Quantity', validators=[InputRequired(), NumberRange(min=1)])
+    submit = SubmitField('Update Quantity')
+
+@bp.route('/', methods=['GET', 'POST'])
+def index():
+    if current_user.is_authenticated:
+        cart = Cart.get_cart(current_user.id)
+        totalPrice = sum(map(lambda x: x.quantity * x.price, cart))
+        quantity_form = QuantityForm()
+        quantity_form.pid.choices = [(item.pid, item.product_name) for item in cart]
+        quantity_form.sid.choices = [(item.sid, item.seller_name) for item in cart]
+        if quantity_form.validate_on_submit():
+            update = Cart.update_item_quantity(current_user.id, quantity_form.pid.data, quantity_form.sid.data, quantity_form.new_quantity.data)
+            if update == 1:
+                return redirect(url_for('cart.index'))
+            else:
+                flash(update)
+    else:
+        cart = None
+        totalPrice = 0
+        quantity_form = None
+    return render_template('cart.html', cart=cart, totalPrice=totalPrice, quantity_form=quantity_form)
+
 @bp.route('/add_product', methods=['POST'])
 def add_product():
-    print(request.form)
     pid = request.form['pid']
     sid = request.form['seller']
     quantity = request.form['quantity']
@@ -18,4 +48,9 @@ def add_product():
         else:
             flash(result)
     return redirect(request.referrer)
-    
+
+@bp.route('/remove_product', methods=['POST'])
+def remove_product():
+    if current_user.is_authenticated:
+        Cart.remove_item_from_cart(current_user.id, request.form['pid'], request.form['sid'])
+    return redirect(url_for('cart.index'))
--- app/models/cart.py
+++ app/models/cart.py
@@ -3,19 +3,16 @@ from flask import current_app as app
 from sqlalchemy import text
 from sqlalchemy.exc import SQLAlchemyError
 
-from app.models.purchase import Purchase
-from app.models.user import User
-
 class CartItem:
-    def __init__(self, pid, sid, quantity):
+    def __init__(self, pid, sid, product_name, seller_name, quantity, price):
         self.pid = pid
         self.sid = sid
+        self.product_name = product_name
+        self.seller_name = seller_name
         self.quantity = quantity
+        self.price = price
 
 class Cart:
-    def __init__(self, cartItems = []):
-        self.cartItems = cartItems
-
     @staticmethod
     def add_item_to_cart(uid, pid, sid, quantity):
         try:
@@ -34,19 +31,53 @@ class Cart:
                 else:
                     return 'Choose a lower quantity.'
         except SQLAlchemyError as e:
-            print(e)
             return 'Item already in cart.'
         return result.rowcount
 
     @staticmethod
     def get_cart(uid):
         rows = app.db.execute('''
-            SELECT pid, sid, quantity
-            FROM Cart
-            WHERE uid = :uid
+            SELECT C.pid AS pid,C.sid AS sid, C.quantity AS quantity, P.price AS price, P.name AS name,
+            U.firstname AS firstname, U.lastname AS lastname
+            FROM Cart C, Products P, Users U
+            WHERE C.uid = :uid AND C.pid = P.id AND U.id = C.sid
         ''', uid=uid)
-        cart = Cart()
+        result = []
         for row in rows:
-            cart.cartItems.append(CartItem(row['pid'], row['sid'], row['quantity']))
-        return cart
+            result.append(CartItem(row['pid'], row['sid'], row['name'], row['firstname'] + ' ' + row['lastname'], 
+                row['quantity'], row['price']))
+        return result
+
+    @staticmethod
+    def remove_item_from_cart(uid, pid, sid):
+        result = app.db.execute('''
+            DELETE FROM Cart
+            WHERE uid = :uid AND pid = :pid AND sid = :sid
+        ''', uid=uid, pid=pid, sid=sid)
+        return result
+
+    @staticmethod
+    def update_item_quantity(uid, pid, sid, new_quantity):
+        if(new_quantity <= 0):
+            return 'Choose a quantity > 0.'
+        try:
+             with app.db.engine.begin() as conn:
+                seller_quantity = conn.execute(text('''
+                    SELECT quantity
+                    FROM Inventory
+                    WHERE uid = :sid AND pid = :pid
+                '''), sid=sid, pid=pid)
+                seller_quantity = seller_quantity.fetchone()
+                if seller_quantity == None:
+                    return 'Choose a valid item in your cart.'
+                if len(seller_quantity) > 0 and int(new_quantity) <= seller_quantity[0]:
+                    result = conn.execute(text('''
+                        UPDATE Cart SET quantity = :new_quantity
+                        WHERE uid = :uid AND pid = :pid AND sid = :sid
+                    '''), uid=uid, pid=pid, sid=sid, new_quantity=new_quantity)
+                else:
+                    return 'Choose a lower quantity.'
+        except SQLAlchemyError as e:
+            return 'Unable to update cart.'
+        return result.rowcount
 

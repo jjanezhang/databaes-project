c8b347dd68cd8b44161c967ee69cbc0e8b667577
Roy Xiong <roy.xiong.1@gmail.com>
2022-04-11 14:26:32 -0400

    Place order

--- db/create.sql
+++ db/create.sql
@@ -7,7 +7,7 @@ CREATE TABLE Users (
     password VARCHAR(255) NOT NULL,
     firstname VARCHAR(255) NOT NULL,
     lastname VARCHAR(255) NOT NULL,
-    balance DECIMAL(12,2) NOT NULL
+    balance DECIMAL(12,2) NOT NULL CHECK (balance >= 0)
 );
 
 CREATE TABLE Products (

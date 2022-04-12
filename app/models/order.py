712d39326465c29636724470dd06e15761509a49
Roy Xiong <roy.xiong.1@gmail.com>
2022-04-12 17:42:14 -0400

    Finish order table

--- app/models/order.py
+++ app/models/order.py
@@ -20,18 +20,19 @@ class Order:
         rows = app.db.execute('''
             SELECT O.id AS oid, O.uid as uid, O.time_placed AS time_placed, P.pid AS pid, 
             P.fulfilled AS fulfilled, P.time_fulfilled AS time_fulfilled, 
-            P.quantity AS quantity, P.price AS price, Pr.name AS product_name
-            FROM Orders O, Purchases P, Products Pr
-            WHERE O.id = P.oid AND O.uid = :uid AND Pr.id = P.pid
+            P.quantity AS quantity, P.price AS price, Pr.name AS product_name,
+            CONCAT(firstname, ' ', lastname) AS seller_name
+            FROM Orders O, Purchases P, Products Pr, Users U
+            WHERE O.id = P.oid AND O.uid = :uid AND Pr.id = P.pid AND U.id = P.sid
             ORDER BY O.time_placed DESC
         ''', uid=uid)
         result = {}
         for row in rows:
             if row.oid in result:
-                result[row.oid].purchases.append(Purchase(row.oid, row.pid, uid, row.fulfilled, row.time_fulfilled, row.quantity, row.price, row.product_name, row.time_placed))
+                result[row.oid].purchases.append(Purchase(row.oid, row.pid, uid, row.fulfilled, row.time_fulfilled, row.quantity, row.price, row.product_name, row.time_placed, row.seller_name))
             else:
                 result[row.oid] = Order(row.oid, row.uid, row.time_placed)
-                result[row.oid].purchases.append(Purchase(row.oid, row.pid, uid, row.fulfilled, row.time_fulfilled, row.quantity, row.price, row.product_name, row.time_placed))
+                result[row.oid].purchases.append(Purchase(row.oid, row.pid, uid, row.fulfilled, row.time_fulfilled, row.quantity, row.price, row.product_name, row.time_placed, row.seller_name))
 
         for key in result:
             result[key].buyer = User.get(result[key].uid)
@@ -49,13 +50,17 @@ class Order:
             WHERE O.id = P.oid AND P.sid = :uid AND Pr.id = P.pid
             ORDER BY O.time_placed DESC
         ''', uid=uid)
+
+        seller = User.get(uid)
+        seller_name = seller.firstname + " " + seller.lastname
+
         result = {}
         for row in rows:
             if row.oid in result:
-                result[row.oid].purchases.append(Purchase(row.oid, row.pid, uid, row.fulfilled, row.time_fulfilled, row.quantity, row.price, row.product_name, row.time_placed))
+                result[row.oid].purchases.append(Purchase(row.oid, row.pid, uid, row.fulfilled, row.time_fulfilled, row.quantity, row.price, row.product_name, row.time_placed, seller_name))
             else:
                 result[row.oid] = Order(row.oid, row.uid, row.time_placed)
-                result[row.oid].purchases.append(Purchase(row.oid, row.pid, uid, row.fulfilled, row.time_fulfilled, row.quantity, row.price, row.product_name, row.time_placed))
+                result[row.oid].purchases.append(Purchase(row.oid, row.pid, uid, row.fulfilled, row.time_fulfilled, row.quantity, row.price, row.product_name, row.time_placed, seller_name))
 
         for key in result:
             result[key].buyer = User.get(result[key].uid)
@@ -78,15 +83,16 @@ class Order:
         rows = app.db.execute('''
             SELECT O.id AS oid, O.uid AS uid, O.time_placed AS time_placed, P.sid AS sid, P.pid AS pid, 
             P.fulfilled AS fulfilled, P.time_fulfilled AS time_fulfilled, 
-            P.quantity AS quantity, P.price AS price, Pr.name AS product_name
-            FROM Orders O, Purchases P, Products Pr
-            WHERE O.id = :oid AND uid = :uid AND O.id = P.oid AND O.uid = :uid AND Pr.id = P.pid
+            P.quantity AS quantity, P.price AS price, Pr.name AS product_name,
+            CONCAT(firstname, ' ', lastname) AS seller_name
+            FROM Orders O, Purchases P, Products Pr, Users U
+            WHERE O.id = :oid AND O.id = P.oid AND O.uid = :uid AND Pr.id = P.pid AND U.id = P.sid
         ''', oid=oid, uid=uid)
 
         if len(rows) > 0:
             result = Order(rows[0].oid, uid, rows[0].time_placed)
             for row in rows:
-                result.purchases.append(Purchase(row.oid, row.pid, row.sid, row.fulfilled, row.time_fulfilled, row.quantity, row.price, row.product_name, row.time_placed))
+                result.purchases.append(Purchase(row.oid, row.pid, row.sid, row.fulfilled, row.time_fulfilled, row.quantity, row.price, row.product_name, row.time_placed, row.seller_name))
             user = User.get(uid)
             result.buyer = user
             return result
--- app/models/purchase.py
+++ app/models/purchase.py
@@ -2,7 +2,7 @@ from flask import current_app as app
 
 
 class Purchase:
-    def __init__(self, oid, pid, sid, fulfilled, time_fulfilled, quantity, price, product_name, time_purchased):
+    def __init__(self, oid, pid, sid, fulfilled, time_fulfilled, quantity, price, product_name, time_purchased, seller_name):
         self.oid = oid
         self.pid = pid
         self.sid = sid
@@ -12,15 +12,17 @@ class Purchase:
         self.price = price
         self.product_name = product_name
         self.time_purchased = time_purchased
+        self.seller_name = seller_name
 
     @staticmethod
     def get_all_by_uid_since(uid, since):
         rows = app.db.execute('''
             SELECT P.oid AS oid, P.pid AS pid, P.sid AS sid, P.fulfilled AS fulfilled,
             P.time_fulfilled AS time_fulfilled, P.quantity AS quantity,
-            P.price AS price, Pr.name AS product_name, O.time_placed AS time_purchased
-            FROM Purchases P, Orders O, Products Pr
-            WHERE O.id = P.oid AND O.uid = :uid AND P.pid = Pr.id
+            P.price AS price, Pr.name AS product_name, O.time_placed AS time_purchased,
+            CONCAT(U.firstname, ' ', U.lastname) AS seller_name
+            FROM Purchases P, Orders O, Products Pr, Users U
+            WHERE O.id = P.oid AND O.uid = :uid AND P.pid = Pr.id AND U.id = P.sid
             AND O.time_placed >= :since
             ORDER BY O.time_placed DESC
             '''
@@ -36,9 +38,10 @@ class Purchase:
         rows = app.db.execute('''
             SELECT P.oid AS oid, P.pid AS pid, P.sid AS sid, P.fulfilled AS fulfilled,
             P.time_fulfilled AS time_fulfilled, P.quantity AS quantity,
-            P.price AS price, Pr.name AS product_name, O.time_placed AS time_purchased
-            FROM Purchases P, Orders O, Products Pr
-            WHERE O.id = P.oid AND O.uid = :uid AND P.pid = :product_id 
+            P.price AS price, Pr.name AS product_name, O.time_placed AS time_purchased,
+            CONCAT(firstname, ' ', lastname) AS seller_name
+            FROM Purchases P, Orders O, Products Pr, Users U
+            WHERE O.id = P.oid AND O.uid = :uid AND P.pid = :product_id AND U.id = P.sid
             AND P.pid = Pr.id AND P.fulfilled=true
             '''
             , uid=uid, product_id=product_id)
